7815ab494f91aa56ba7c54658fcfc3cfa829b9d4
villagrat <elsainetedelautrec@gmail.com>
2022-04-09 08:54:51 -0300

    added auth middleware and protected user route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,11 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
 const User = require('../models/userModel');
 
 // @desc   Register a new user
 // @route  /api/users
+// @method POST
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -49,6 +49,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // @desc   Login a user
 // @route  /api/users/login
+// @method POST
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -69,6 +70,20 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   GET current user
+// @route  /api/users/me
+// @method GET
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+
+  res.status(200).json(user);
+});
+
 // Generate token
 const generateToken = (id) => {
   return jwt.sign(
@@ -85,4 +100,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
