2f8dd67347d946d0550f818d11cf642ac3228853
villagrat <elsainetedelautrec@gmail.com>
2022-04-09 06:45:21 -0300

    added db setup and basic error handler

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
+// connect to db
+connectDB();
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.status(201).json({
+  res.status(200).json({
     message: 'Welcome to the Help Desk API',
   });
 });
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
